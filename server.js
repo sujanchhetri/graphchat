51780408c32ac215cbb67cde7c024253f59d4404
asuju <zoranhaude@gmail.com>
2021-02-28 20:16:26 +0530

    db connected,schema refactored

---
+++ server.js
@@ -0,0 +1,21 @@
+const { ApolloServer } = require("apollo-server");
+
+const {sequelize} = require('./models')
+
+const typeDefs = require('./graphql/typeDefs')
+const resolvers = require('./graphql/resolvers');
+
+
+const server = new ApolloServer({
+	typeDefs,
+	resolvers,
+});
+
+server.listen().then(({ url }) => {
+	console.log(`🚀 Server ready at ${url}`);
+
+    sequelize
+     .authenticate()
+     .then(()=> console.log('DB connected'))
+     .catch((err)=> console.log(err))
+});
