cbccb5aaad2560a1d9c76c7902356220eab49dac
asuju <zoranhaude@gmail.com>
2021-03-04 15:46:42 +0530

    client side validation

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,34 @@
-import React from "react";
-import { Container } from "react-bootstrap";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import React from 'react'
+import { Container } from 'react-bootstrap'
+import { BrowserRouter, Switch } from 'react-router-dom'
 
-import ApolloProvider from "./ApolloProvider";
+import ApolloProvider from './ApolloProvider'
 
-import "./App.scss";
-import Home from "./pages/Home";
-import Register from "./pages/Register";
-import Login from "./pages/Login";
+import './App.scss'
+
+import Home from './pages/Home'
+import Register from './pages/Register'
+import Login from './pages/Login'
+
+import { AuthProvider } from './context/auth'
+import DynamicRoute from './util/DynamicRoute'
 
 function App() {
-	return (
-		<ApolloProvider>
-			<BrowserRouter>
-				<Container className="pt-5">
-					<Switch>
-						<Route path="/register" component={Register} />
-						<Route exact path="/" component={Home} />
-						<Route path="/login" component={Login} />
-					</Switch>
-				</Container>
-			</BrowserRouter>
-		</ApolloProvider>
-	);
+  return (
+    <ApolloProvider>
+      <AuthProvider>
+        <BrowserRouter>
+          <Container className="pt-5">
+            <Switch>
+              <DynamicRoute exact path="/" component={Home} authenticated />
+              <DynamicRoute path="/register" component={Register} guest />
+              <DynamicRoute path="/login" component={Login} guest />
+            </Switch>
+          </Container>
+        </BrowserRouter>
+      </AuthProvider>
+    </ApolloProvider>
+  )
 }
 
-export default App;
+export default App
