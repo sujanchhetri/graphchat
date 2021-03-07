bfb575b29ee4559ddd9cc32a3d856f9a69ef4067
asuju <zoranhaude@gmail.com>
2021-03-07 21:05:54 +0530

    subscription added

--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -7,9 +7,9 @@ module.exports = gql`
 		createdAt: String!
 		token: String
 		imageUrl: String
-		latestMessage:Message
+		latestMessage: Message
 	}
-	type Message{
+	type Message {
 		uuid: String!
 		content: String!
 		from: String!
@@ -18,8 +18,8 @@ module.exports = gql`
 	}
 	type Query {
 		getUsers: [User]!
-		login(username: String!,password: String!):User!
-		getMessages(from:String!):[Message]!
+		login(username: String!, password: String!): User!
+		getMessages(from: String!): [Message]!
 	}
 
 	type Mutation {
@@ -29,6 +29,9 @@ module.exports = gql`
 			password: String!
 			confirmPassword: String!
 		): User!
-		sendMessage(to: String! content:String!):Message!
+		sendMessage(to: String!, content: String!): Message!
+	}
+	type Subscription {
+		newMessage: Message!
 	}
 `;
